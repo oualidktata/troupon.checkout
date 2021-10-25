efb6fe5c2ba32a151ed0ae3a7501149ca7ede306
Pedro Ferreira <pedro.ferreira@beslogic.com>
2021-10-25 12:11:20 -0400

    Adds gift object to place order as guest

--- Troupon.Checkout.Core.Application/DTOs/PlaceOrderAsGuestDTO.cs
+++ Troupon.Checkout.Core.Application/DTOs/PlaceOrderAsGuestDTO.cs
@@ -2,15 +2,25 @@ using System;
 
 namespace Troupon.Checkout.Core.Application.DTOs
 {
-  public class PlaceOrderAsGuestDto
+  public record PlaceOrderAsGuestDto
   {
     public Guid DealId { get; init; }
-    public string Email { get; init; }
+    public string Email { get; init; } = "";
     public bool SendPromotionalEmails { get; init; }
 
-    public string CardholderName { get; init; }
-    public string CardNumber { get; init; }
-    public string CardExpirationDate { get; init; }
+    public string CardholderName { get; init; } = "";
+    public string CardNumber { get; init; } = "";
+    public string CardExpirationDate { get; init; } = "";
     public int CVV { get; init; }
+    public GiftDetails? GiftDetails { get; init; }
+  }
+
+  public record GiftDetails
+  {
+    public string ToEmail { get; init; }
+    public string RecipientEmail { get; init; }
+    public string Message { get; init; }
+    public string FromEmail { get; init; }
+    public DateTime EmailDeliveryDate { get; init; }
   }
 }
