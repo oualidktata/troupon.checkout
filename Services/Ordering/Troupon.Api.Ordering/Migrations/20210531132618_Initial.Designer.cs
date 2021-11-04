8598fa3001d6d80c6ba0c237513c0a2320fb0e14
Pedro Ferreira <pedro.ferreira@beslogic.com>
2021-11-04 17:46:02 -0400

    fix namespace on migrations

--- Services/Ordering/Troupon.Api.Ordering/Migrations/20210531132618_Initial.Designer.cs
+++ Services/Ordering/Troupon.Api.Ordering/Migrations/20210531132618_Initial.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Troupon.Infra.Persistence.Ordering;
+using Troupon.Infrastructure.Ordering;
 
 namespace Troupon.Api.Ordering.Migrations
 {
